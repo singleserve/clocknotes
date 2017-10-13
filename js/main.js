e43c1a826f55ef3b3f221059e190bd5c628717e3
Syed Abdullah <contact@syedabdullah.net>
2017-10-13 13:28:17 +0800

    site: basic layout + working clock

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+function zeroPad(t) {
+    return (t < 10) ? "0" + t : t;
+}
 
+function initTimer(clockID) {
+    var now = new Date();
+
+    var h = now.getHours();
+    var m = zeroPad(now.getMinutes());
+    var s = zeroPad(now.getSeconds());
+
+    document.getElementById(clockID).innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(function() {
+        initTimer(clockID);
+    }, 500);
+}
+
+initTimer("clock");
