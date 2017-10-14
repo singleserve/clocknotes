9134657eb42d0394d32f1aefd5c91636e74b4d1d
Syed <syed@syedabdullah.net>
2017-10-14 15:24:49 +0800

    notes: allow notes to persist across browser sessions (#4)

--- js/main.js
+++ js/main.js
@@ -10,15 +10,32 @@ function initTimer(clockID) {
     var s = zeroPad(now.getSeconds());
 
     document.getElementById(clockID).innerHTML = h + ":" + m + ":" + s;
-    var t = setTimeout(function() {
+    var t = window.setTimeout(function() {
         initTimer(clockID);
     }, 500);
 }
 
-initTimer("clock");
+function autoSaveRunner(editor) {
+    window.localStorage.setItem('editor', editor.getData());
+    window.setTimeout(function() {
+        autoSaveRunner(editor);
+    }, 500);
+}
 
-InlineEditor
-.create( document.querySelector('#notes'))
-.catch( error => {
-    console.error( error );
-} );
+function createEditor() {
+    InlineEditor
+    .create(document.querySelector('#notes'))
+    .then(editor => {
+        var data = window.localStorage.getItem('editor');
+        if (data != null) {
+            editor.setData(data);
+        }
+        autoSaveRunner(editor);
+    })
+    .catch(error => {
+        console.error( error );
+    });
+}
+
+initTimer("clock");
+createEditor();
